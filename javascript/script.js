3b79f3536dd12fe8cdeed58ef3c9a846836b95e3
Carlos <cshaw.prsvr@gmail.com>
2022-11-09 20:54:58 -0600

    11-9-22

--- javascript/script.js
+++ javascript/script.js
@@ -1,20 +1,31 @@
-let firstCard = getRandomCard()
-let secondCard = getRandomCard()
-let cards = [firstCard, secondCard]
-let sum = firstCard + secondCard
+let cards = []
+let sum = 0
 let hasBlackJack = false
-let isAlive = true
+let isAlive = false
 let message = ""
 let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
 
 function getRandomCard() {
-     return 5
-}
+
+     let randomNumber = Math.floor(Math.random * 13) + 1
+     if (randomNumber > 10) {
+        return 10
+     }else if (randomNumber === 1) {
+        return 11
+     }else {
+        return randomNumber
+     }
+    }
 
 function startGame() { 
-    renderGame
+isAlive = true
+let firstCard = getRandomCard()
+let secondCard = getRandomCard()
+cards = [firstCard, secondCard]
+sum = firstCard + secondCard
+    renderGame()
 }
 
 function renderGame() {
@@ -39,8 +50,7 @@ function newCard() {
     let card = getRandomCard()
     sum += card
     cards.push(card)
-    console.log(cards)
+    console.log(card)
     renderGame()
 }
-
 
